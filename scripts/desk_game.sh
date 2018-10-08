fb608629dca57c2d0293177b78e3914851a7fab2
Clément Aymard <blog.clm@gmail.com>
2018-10-08 11:57:54 +0200

    clean script file extension

---
+++ scripts/desk_game.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+xrandr \
+  --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --brightness 0.9 --primary \
+  --output eDP1 --off \
+  --output DP1 --off
--- scripts/desk_pres.sh
+++ scripts/desk_pres.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 xrandr \
   --output eDP1  --mode 1920x1080 --pos 0x0    --rotate normal --primary \
-  --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal
+  --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal \
+  --output DP1 --off
