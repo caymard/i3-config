4ea18ab581d509972cb13ba8300bd9ead154865e
Clément Aymard <blog.clm@gmail.com>
2018-11-26 11:54:42 +0100

    gitlab MR displayed are now me and SUD

--- scripts/gitlab_mr.py
+++ scripts/gitlab_mr.py
@@ -1,20 +1,30 @@
+import argparse
+
 import requests
 
 from settings import GITLAB_PRIVATE_TOKEN
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Fetch Gitlab API about opened MR.')
+    parser.add_argument('--target', metavar='TARGET', type=str, default='me', help='the target to fetch MR from')
+    args = parser.parse_args()
+
     if GITLAB_PRIVATE_TOKEN is None:
         raise ValueError('GITLAB_PRIVATE_TOKEN is None, stopping here.')
 
+    request_params = {'state': 'opened'}
+    if args.target == 'sud':
+        request_params['scope'] = 'all'
+        request_params['labels'] = 'SUD'
+    elif args.target == 'me':
+        request_params['scope'] = 'assigned_to_me'
+    
     r = requests.get(
         'https://gitlab.citymeo.fr/api/v4/groups/5/merge_requests',
         headers={
             'Private-Token': GITLAB_PRIVATE_TOKEN,
         },
-        params={
-            'state': 'opened',
-            'scope': 'assigned_to_me',
-        },
+        params=request_params,
     )
 
     mr_count = int(r.headers.get('X-Total', '?'))
