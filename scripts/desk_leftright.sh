9b09fd658bbb5ecaa933a0ac4bf73895dd8b9db0
Clément Aymard <blog.clm@gmail.com>
2019-02-04 15:24:44 +0100

    improve left-right pattern

--- scripts/desk_leftright.sh
+++ scripts/desk_leftright.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 xrandr \
-  --output eDP1 --mode 1920x1080 --pos 0x850 --rotate normal \
+  --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal \
   --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --brightness 0.9 --primary
