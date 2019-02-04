6909475214748c60f795ddba93ebca9130486e4e
Clément Aymard <blog.clm@gmail.com>
2019-02-04 15:24:26 +0100

    webapp status in i3blocks

---
+++ scripts/webapp_status.py
@@ -0,0 +1,27 @@
+import requests
+
+STATUS = {
+    'off': {
+        'message': '🔴',
+    },
+    'starting': {
+        'message': '🔺',
+    },
+    'running': {
+        'message': '🔵',
+    },
+}
+
+try:
+    r = requests.get('http://clement.citymeo.lan/api/management/v2/', timeout=1)
+except requests.exceptions.ConnectionError:
+    status = 'off'
+else:
+    if r.status_code == 502:
+        status = 'starting'
+    elif r.status_code == 403:
+        status = 'running'
+    else:
+        status = 'off'
+finally:
+    print(STATUS[status]['message'])
