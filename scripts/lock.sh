e49851622c5f0294eab8fa0868cbd81a0565c751
Clément Aymard <blog.clm@gmail.com>
2019-02-04 15:22:56 +0100

    improve locking

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # shellcheck disable=SC2086
-i3lock -f -t -i $1
+# i3lock -f -t -i /home/caymard/.config/i3/wallpapers/space.png
+/home/caymard/.config/i3/scripts/betterlockscreen -l blur
