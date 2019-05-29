72acb067837de81bf45a8b4d59c78c2ce8a8496c
Clément Aymard <blog.clm@gmail.com>
2019-05-29 07:48:17 +0200

    change screen config for home

--- scripts/desk_home.sh
+++ scripts/desk_home.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 xrandr \
-  --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --brightness 0.9 --primary \
-  --output eDP1 --mode 1920x1080 --pos 1920x0 --rotate normal
+  --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --brightness 0.9 \
+  --output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal --primary
