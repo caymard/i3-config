467d91228392350e13565e60d69f2703bc01baae
Clément Aymard <blog.clm@gmail.com>
2019-06-17 16:47:44 +0200

    welcome back masterrace

--- scripts/desk_masterrace.sh
+++ scripts/desk_masterrace.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 xrandr \
-  --output eDP1  --mode 1920x1080 --pos 1920x0 --rotate normal \
-  --output DP1   --mode 1920x1080 --pos 0x0    --rotate normal --brightness 0.9  --primary
+  --output HDMI2 --mode 1920x1080 --pos 0x0    --rotate normal --brightness 0.9 \
+  --output DP1   --mode 1920x1080 --pos 1920x0 --rotate normal --brightness 0.9 --primary \
+  --output eDP1  --mode 1920x1080 --pos 3840x0 --rotate normal
