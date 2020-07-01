0782f6a067041e6ff2ce19dfc168d9dee240242c
Clément Aymard <aymard.clement@gmail.com>
2020-07-01 13:55:25 +0200

    update installation

--- install.sh
+++ install.sh
@@ -2,20 +2,20 @@
 set -e
 
 mkdir -p ~/.venvs/i3
-virtualenv -p python3 ~/.venvs/i3
+python3 -m venv ~/.venvs/i3
 # shellcheck disable=SC1090
 source ~/.venvs/i3/bin/activate
 pip install -Ur requirements.txt
 deactivate
 
+sudo add-apt-repository ppa:kgilmer/speed-ricer
 sudo apt update -qq
 sudo apt install --yes \
 	xbacklight \
 	i3 \
+	i3-gaps \
 	i3blocks \
 	rofi
-
-echo "⚠ I installed i3, but if you want fancy gaps go install i3-gaps"
 
 mkdir -p ~/.config/rofi
 cp -r rofi/* ~/.config/rofi
